e10c3c81d50abde0d9557700b74786633e0a6352
Gyanappa dalavai <138701830+Gyanappadalavai@users.noreply.github.com>
2025-04-24 20:24:37 +0530

    Update apb_sequencer.sv

--- apb_sequencer.sv
+++ apb_sequencer.sv
@@ -1,1 +1,15 @@
 
+//import uvm_pkg::*;
+//`include "uvm_macros.svh"
+//`include "apb_seq_item.sv"
+class apb_sequencer extends uvm_sequencer#(apb_sequence_item);
+
+	//Factory registration
+	`uvm_component_utils(apb_sequencer)
+ 
+	//Constructor
+	function new (string name="apb_sequencer",uvm_component parent=null);
+		super.new(name,parent);
+	endfunction
+
+endclass: apb_sequencer
