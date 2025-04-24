a8e3e157780dd8801ad4c02cdcd3ecbe4baa51ec
Gyanappa dalavai <138701830+Gyanappadalavai@users.noreply.github.com>
2025-04-24 12:26:21 +0530

    Update apb_interface.sv

--- apb_interface.sv
+++ apb_interface.sv
@@ -1,1 +1,11 @@
+interface apb_intf(input bit pclk, presetn);
+  logic  transfer;
+  logic read_write;
+  logic apb_write_paddr[`AW-1:0];
+  logic apb_read_paddr[`AW-1:0];
+  logic apb_write_data[`DW-1:0];
+  logic apb_read_data_out[`DW-1:0];
+
+
+endinterface
 
